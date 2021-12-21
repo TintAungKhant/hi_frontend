edc6bc10cca233e6eb1a964641fbc19f66f01ac5
TintAungKhant <aherkersoemin@gmail.com>
2021-12-21 20:41:35 +0630

    added api calls and functions to friends page

--- src/pages/friends/Friends.jsx
+++ src/pages/friends/Friends.jsx
@@ -1,166 +1,183 @@
-import React from 'react'
+import React, { Component } from "react";
+import _ from "lodash";
+import {
+  getContacts as apiGetContacts,
+  postAcceptContact,
+  postDeleteContact,
+} from "../../api";
+import Contact from "../../components/contact/Contact";
 
-function Friends() {
-  return (
-    <section className="container py-4">
-    <div className="card mb-3">
-      <div className="card__header">
-        <h2>Pending</h2>
-      </div>
-      <div className="card__body">
-        <ul className="list">
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
+class Friends extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contacts_request: [],
+      contacts_online: [],
+      contacts_offline: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getContacts("request");
+    this.getContacts("online");
+    this.getContacts("offline");
+  }
+
+  getContacts = (type) => {
+    apiGetContacts({ type }).then((res) => {
+      switch (type) {
+        case "request":
+          this.setState({
+            ...this.state,
+            contacts_request: res.data.data.contacts,
+          });
+          break;
+
+        case "online":
+          this.setState({
+            ...this.state,
+            contacts_online: res.data.data.contacts,
+          });
+          break;
+
+        case "offline":
+          this.setState({
+            ...this.state,
+            contacts_offline: res.data.data.contacts,
+          });
+          break;
+
+        default:
+          break;
+      }
+    });
+    // TODO:: handle error
+  };
+
+  accept = (id) => {
+    postAcceptContact(id).then(() => {
+      let contacts_request = _.filter(
+        this.state.contacts_request,
+        (contact) => {
+          return contact.id !== id;
+        }
+      );
+      this.setState({ ...this.state, contacts_request });
+      this.getContacts("online");
+      this.getContacts("offline");
+    });
+    // TODO:: handle error
+  };
+
+  remove = (id, type) => {
+    postDeleteContact(id).then(() => {
+      let new_contacts;
+      switch (type) {
+        case "request":
+          new_contacts = _.filter(this.state.contacts_request, (contact) => {
+            return contact.id !== id;
+          });
+          this.setState({ ...this.state, contacts_request: new_contacts });
+          break;
+
+        case "online":
+          new_contacts = _.filter(this.state.contacts_online, (contact) => {
+            return contact.id !== id;
+          });
+          this.setState({ ...this.state, contacts_online: new_contacts });
+          break;
+
+        case "offline":
+          new_contacts = _.filter(this.state.contacts_offline, (contact) => {
+            return contact.id !== id;
+          });
+          this.setState({ ...this.state, contacts_offline: new_contacts });
+          break;
+
+        default:
+          break;
+      }
+    });
+    // TODO:: handle error
+  };
+
+  render() {
+    return (
+      <section className="container py-4">
+        {this.state.contacts_request.length ? (
+          <div className="card mb-3">
+            <div className="card__header">
+              <h2>Pending</h2>
             </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
+            <div className="card__body">
+              <ul className="list">
+                {this.state.contacts_request.map((contact) => {
+                  return (
+                    <Contact
+                      type={"request"}
+                      key={contact.id}
+                      id={contact.id}
+                      accept={this.accept}
+                      remove={this.remove}
+                    />
+                  );
+                })}
+              </ul>
             </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Accept</button>
-              <button className="btn btn--light-red">Reject</button>
+          </div>
+        ) : (
+          <></>
+        )}
+        {this.state.contacts_online.length ? (
+          <div className="card mb-3">
+            <div className="card__header">
+              <h2>Online</h2>
             </div>
-          </li>
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
+            <div className="card__body">
+              <ul className="list">
+                {this.state.contacts_online.map((contact) => {
+                  return (
+                    <Contact
+                      type={"online"}
+                      key={contact.id}
+                      id={contact.id}
+                      remove={this.remove}
+                    />
+                  );
+                })}
+              </ul>
             </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
+          </div>
+        ) : (
+          <></>
+        )}
+        {this.state.contacts_offline.length ? (
+          <div className="card mb-3">
+            <div className="card__header">
+              <h2>Offline</h2>
             </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Accept</button>
-              <button className="btn btn--light-red">Reject</button>
+            <div className="card__body">
+              <ul className="list">
+                {this.state.contacts_offline.map((contact) => {
+                  return (
+                    <Contact
+                      type={"offline"}
+                      key={contact.id}
+                      id={contact.id}
+                      remove={this.remove}
+                    />
+                  );
+                })}
+              </ul>
             </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div className="card mb-3">
-      <div className="card__header">
-        <h2>Online</h2>
-      </div>
-      <div className="card__body">
-        <ul className="list">
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
-            </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
-              <div className="list__item__content__text">
-                <span className="online-badge">Online</span>
-              </div>
-            </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Message</button>
-              <button className="btn btn--light-red">Unfriend</button>
-            </div>
-          </li>
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
-            </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
-              <div className="list__item__content__text">
-                <span className="online-badge">Online</span>
-              </div>
-            </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Message</button>
-              <button className="btn btn--light-red">Unfriend</button>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div className="card mb-3">
-      <div className="card__header">
-        <h2>Offline</h2>
-      </div>
-      <div className="card__body">
-        <ul className="list">
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
-            </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
-              <div className="list__item__content__text">
-                <span className="offline-badge">Offline</span>
-              </div>
-            </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Message</button>
-              <button className="btn btn--light-red">Unfriend</button>
-            </div>
-          </li>
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
-            </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
-              <div className="list__item__content__text">
-                <span className="offline-badge">Offline</span>
-              </div>
-            </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Message</button>
-              <button className="btn btn--light-red">Unfriend</button>
-            </div>
-          </li>
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
-            </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
-              <div className="list__item__content__text">
-                <span className="offline-badge">Offline</span>
-              </div>
-            </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Message</button>
-              <button className="btn btn--light-red">Unfriend</button>
-            </div>
-          </li>
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
-            </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
-              <div className="list__item__content__text">
-                <span className="offline-badge">Offline</span>
-              </div>
-            </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Message</button>
-              <button className="btn btn--light-red">Unfriend</button>
-            </div>
-          </li>
-          <li className="list__item">
-            <div className="list__item__image">
-              <img src="https://source.unsplash.com/500x500/?selfie" alt="Tom"/>
-            </div>
-            <div className="list__item__content">
-              <div className="list__item__content__title">Your Name</div>
-              <div className="list__item__content__text">
-                <span className="offline-badge">Offline</span>
-              </div>
-            </div>
-            <div className="list__item_action">
-              <button className="btn btn--light-purple">Message</button>
-              <button className="btn btn--light-red">Unfriend</button>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </section>
-  )
+          </div>
+        ) : (
+          <></>
+        )}
+      </section>
+    );
+  }
 }
 
-export default Friends
+export default Friends;
